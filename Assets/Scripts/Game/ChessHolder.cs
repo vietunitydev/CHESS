a82aac5ae35175ace32c9aee202cb00eb8ffdbe4
viet <vietunitydev2222@gmail.com>
2024-09-28 02:46:39 +0700

    - chess holder (implement later)

---
+++ Assets/Scripts/Game/ChessHolder.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessHolder : MonoBehaviour
+{
+    public ChessPieceBase Chess { get; set; }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
